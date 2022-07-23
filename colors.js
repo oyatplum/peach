aff1f8c3190173bded4e9edc205a5367ecac3cc8
oyatplum <108467989+oyatplum@users.noreply.github.com>
2022-07-23 10:54:40 +0900

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,29 @@
+var Links = {
+    SetColor:function (color){
+        var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+        console.log(alist[i]);
+        alist[i].style.color = color;
+        i = i + 1;
+    }
+}
+}
+var Body = {
+    SetColor:function (color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+function dark_bright_handler(self){
+    var target = document.querySelector('body');
+if(self.value === 'dark'){
+    Body.SetColor('rgb(218, 132, 128)');
+    self.value = 'bright';
+    Links.SetColor('black');
+    }
+else{
+    Body.SetColor('rgb(237, 172, 169)');
+    self.value = 'dark';
+    Links.SetColor('white');
+    }
+}
